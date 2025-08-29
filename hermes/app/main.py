02d9923c8e23a708c81e97e1a7f191ac10d68ecf
CtrlAltGiri <giridhar.balaz@gmail.com>
2025-08-29 19:36:48 -0400

    setting up openai clients

--- hermes/app/main.py
+++ hermes/app/main.py
@@ -1,8 +1,30 @@
-from agents import Agent
-from agents.tool import HostedMCPTool, Mcp
+from openai import AsyncAzureOpenAI
+from agents import set_default_openai_client
+from agents import Agent, Runner
+from dotenv import load_dotenv
+load_dotenv()
 
-def main():
-    print("Hello, world!")
+import asyncio
+import os
+
+custom_client = AsyncAzureOpenAI(
+    azure_endpoint=os.getenv("OPENAI_BASE_URL", ""),
+    api_key=os.getenv("OPENAI_API_KEY"),
+    api_version=os.getenv("OPENAI_API_VERSION")
+)
+
+set_default_openai_client(custom_client)
+
+async def main():
+    agent = Agent(
+        name="Math Tutor",
+        instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
+        model="gpt-5-nano"
+    )
+    
+    result = await Runner.run(agent, "What is the capital of France?s")
+
+    return result.final_output
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
